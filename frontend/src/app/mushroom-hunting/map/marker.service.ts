c0f08129d00cdcc9c493246c1e505b9c765e42cd
Tomasz Gryczka <tumek001@gmail.com>
2023-11-16 22:45:38 +0100

    add image as marker

--- frontend/src/app/mushroom-hunting/map/marker.service.ts
+++ frontend/src/app/mushroom-hunting/map/marker.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Icon, LayerGroup, Marker, MarkerOptions} from 'leaflet';
+import {DivIcon, FeatureGroup, Icon, LayerGroup, Marker, MarkerOptions} from 'leaflet';
 import {Mushroom} from "../../shared/model/mushroom";
 
 @Injectable({
@@ -10,19 +10,24 @@ export class MarkerService {
   constructor() {
   }
 
-  markMushroomsOnMap(mushrooms: Mushroom[], markers?: LayerGroup): void {
+  markMushroomsOnMap(mushrooms: Mushroom[], markers?: FeatureGroup): void {
     if (markers) {
-      const icon = new Icon.Default();
-      icon.options.shadowSize = [0, 0];
 
       mushrooms.forEach(mushroom => {
         if (mushroom.latitude && mushroom.longitude) {
           const newMarker = new Marker([mushroom.latitude, mushroom.longitude], {
-            icon: icon
+            icon: this.circleWithImage(mushroom.imageUrl),
           } as MarkerOptions);
           newMarker.addTo(markers);
         }
       });
     }
+  }
+
+  circleWithImage(imageUrl: string): DivIcon {
+    return new DivIcon({
+      className: 'custom-div-icon',
+      html: `<img src="${imageUrl}" alt="image" style="border-radius: 50%; background-size: cover; width: 50px; height: 50px">`
+    })
   }
 }
